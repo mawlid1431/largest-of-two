91e9789a723ee89f5a75306dbd87456a46e4fdbb
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-07-28 17:08:10 +0300

    done task 2

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let number1 = 27;
+let number2 = 19;
+
+// Step 2: Compare the numbers using an if statement
+if (number1 > number2) {
+  console.log(`The largest number out of the two numbers is ${number1}.`);
+} else if (number2 > number1) {
+  console.log(`The largest number out of the two numbers is ${number2}.`);
+} else {
+  console.log("Both numbers are equal.");
+}
